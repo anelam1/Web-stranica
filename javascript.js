c80fcd2ed98086df8abaf129f27275f0e6f5608f
anelam1 <115664042+anelam1@users.noreply.github.com>
2022-10-12 21:41:30 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+            
+function test2() 
+        {
+          var c = confirm('Da li ste sigurni da želite poništiti?');
+          if(c)
+                document.getElementById("id1").innerHTML ="Hvala vam! Nadamo se da će te nam se opet vratiti";
+          else
+              document.getElementById("id1").innerHTML ="Hvala što nas niste napustili! Nastavite sa upitnikom";
+        }
+function test3() 
+            {
+             window.alert(" Hvala vam što ste nas kontaktirali, potrudit ćemo se da što prije dobijete odgovor na vaš e-mail!  Autoškola Anela ");
+            } 
